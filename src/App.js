07d122456fb89e13c3c3ecea0a39ae05bb82c4e6
farhadmpr <farhadmp@gmail.com>
2020-03-23 18:47:16 +0430

    Add ProductList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import ProductList from './components/ProductList'
 
 function App() {
   return (
     <div className="main">
-      <h2>Shopping Cart</h2>
+      <header className="main-header">
+        <h2>Shopping Cart</h2>        
+      </header>
+      <ProductList />
     </div>
   );
 }
---
+++ src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './ProductList.css'
+
+const ProductList = () => {
+    return (
+        <div className="product-list">
+            <h3>Product List</h3>
+        </div>
+    )
+}
+
+export default ProductList
