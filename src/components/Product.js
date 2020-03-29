1edf8bbe77d7bb9f8bcb3e3c69b4be774b9c23b4
farhadmpr <farhadmp@gmail.com>
2020-03-29 15:37:49 +0430

    Add addToCart function

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,11 +3,21 @@ import React from 'react'
 import './Product.css'
 
 const Product = (props) => {
+
+    const addToCart = () => {
+        console.log('Clicked')
+    }
+
     return(
         <div className="product">
             <p>{ props.title }</p>
             <p>{ props.price }</p>
-            <button>Add to Cart</button>
+            <button
+                className="btn"
+                onClick={addToCart}
+            >
+                Add to Cart
+            </button>
         </div>
     )
 }
