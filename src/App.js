d7028b4ad61bb472acee55fabc8823dd6daf71bb
farhadmpr <farhadmp@gmail.com>
2020-03-29 15:41:16 +0430

    Add cart component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.css';
 import ProductList from './components/ProductList'
+import Cart from './components/Cart'
 
 function App() {
   return (
---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div className="cart">
+            <p>Items in cart: 0</p>
+            <p>Total price: 0</p>
+        </div>
+    )
+}
+
+export default Cart
